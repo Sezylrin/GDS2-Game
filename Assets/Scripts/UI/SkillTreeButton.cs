aec9e8b132b569b53a9aceb0b047e08bfa21cbf5
Edoardo <edoardomenon1997@gmail.com>
2023-09-11 09:46:46 +1000

    Initial controller

---
+++ Assets/Scripts/UI/SkillTreeButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("Clicked Button");
+    }
+}
