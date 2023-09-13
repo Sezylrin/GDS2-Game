fc3959a146a7cd12e1a5b1b8ac9f572362bd65b6
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-09-13 19:11:49 +1000

    Bug fix

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -28,7 +28,7 @@ public class InputController : MonoBehaviour
         player.AbilityTwo.performed += PCM.control.BufferAbilityTwo;
         player.AbilityThree.performed += PCM.control.BufferAbilityThree;
         player.ToggleAbilities.performed += PCM.abilities.ToggleActiveAbilitySet;
-
+        Debug.Log(GameManager.Instance);
         GameManager.Instance.SetPlayerTransform(transform, PCM);
     }
 
