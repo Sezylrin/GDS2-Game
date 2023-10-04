f5174b37c84c54f0cf9e4755ad5e8f831b3b6c9e
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-10-04 19:24:51 +1100

    Bug Fixing

--- Assets/Scripts/Abilities/AbilityShapes/AreaAbility.cs
+++ Assets/Scripts/Abilities/AbilityShapes/AreaAbility.cs
@@ -47,6 +47,7 @@ public class AreaAbility : AbilityBase
         else
         {
             col2D.radius = selected.radius;
+            AOETr.localScale = new Vector3(selected.radius, selected.radius, selected.radius) * 2;
         }
     }
 
@@ -61,7 +62,7 @@ public class AreaAbility : AbilityBase
             if (timer + Time.deltaTime >= selected.growthSpeed)
                 col2D.radius = endRadius;
             float radius = col2D.radius;
-            AOETr.localScale = new Vector3(radius, radius, radius);
+            AOETr.localScale = new Vector3(radius, radius, radius) * 2;
             yield return null;
         }
         StopExpand();
