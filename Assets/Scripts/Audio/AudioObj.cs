8cbc4e13374434878515e9304a5b709bf33e41a0
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-10-06 23:03:05 +1100

    Some more audio options

--- Assets/Scripts/Audio/AudioObj.cs
+++ Assets/Scripts/Audio/AudioObj.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using DG.Tweening;
 
 public class AudioObj : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class AudioObj : MonoBehaviour
     private AudioSource source;
     private bool isPaused = false;
     private AudioManager manager;
+    private float initialVolume;
     void Start()
     {
     }
@@ -34,24 +36,54 @@ public class AudioObj : MonoBehaviour
         source.Play();
         source.loop = loop;
         source.volume = volume;
+        initialVolume = volume;
     }
 
-    public void PauseSound()
+    public void PauseSound(bool fade = false, float dur = 1)
     {
-        source.Pause();
-        isPaused = true;
+        if (!fade)
+        {
+            source.Pause();
+            isPaused = true;
+        }
+        else
+        {
+            source.DOFade(0, dur).OnComplete(() =>
+             {
+                 source.Pause();
+                 isPaused = true;
+             });
+        }
     }
 
-    public void ResumeSound()
+    public void ResumeSound(bool fade = false, float dur = 1)
     {
+        if (!fade) 
+            source.volume = initialVolume;
         source.UnPause();
         isPaused = false;
+        if (fade)
+        {
+            source.DOFade(initialVolume, dur);
+        }
+        
     }
 
-    public void StopSound()
+    public void StopSound(bool fade = false, float dur = 1)
     {
-        source.Stop();
-        OnComplete();
+        if (!fade)
+        {
+            source.Stop();
+            OnComplete();
+        }
+        else
+        {
+            source.DOFade(0, dur).OnComplete(() =>
+            {
+                source.Stop();
+                OnComplete();
+            });
+        }
     }
 
     private void OnComplete()
@@ -60,4 +92,11 @@ public class AudioObj : MonoBehaviour
         isPaused = false;
         manager.ReAddToStack(this);
     }
+
+    public void FadeIn(float startVol, float endVol, float dur)
+    {
+        source.volume = startVol;
+        source.DOFade(endVol, dur);
+    }
+
 }
