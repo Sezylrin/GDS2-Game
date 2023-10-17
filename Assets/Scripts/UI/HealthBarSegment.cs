3fa8f144bd403934927454a05d4b71fbc49130c4
Xero8252 <39457241+Xero-oce@users.noreply.github.com>
2023-10-17 17:30:20 +1100

    Enemy UI Updates

--- Assets/Scripts/UI/HealthBarSegment.cs
+++ Assets/Scripts/UI/HealthBarSegment.cs
@@ -6,16 +6,6 @@ using UnityEngine.UI;
 public class HealthBarSegment : MonoBehaviour
 {
     [field: SerializeField] private Image image;
-    
-    public void SetToGreen()
-    {
-        image.color = Color.green;
-    }
-    
-    public void SetToYellow()
-    {
-        image.color = Color.yellow;
-    }
 
     public void UpdateFillPercent(float percent)
     {
