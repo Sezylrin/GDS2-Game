f9131012eb108ecb88440f9e6461b85e1a750c4d
Edoardo <edoardomenon1997@gmail.com>
2023-11-01 15:29:19 +1100

    Hide player UI when opening menus

--- Assets/Scripts/UI/BookMenu.cs
+++ Assets/Scripts/UI/BookMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.InputSystem;
 using System.Collections;
+using DG.Tweening.Core.Easing;
 
 public class BookMenu : Menu
 {
@@ -40,6 +41,7 @@ public class BookMenu : Menu
         }
         else
         {
+            GameManager.Instance.PlayerTransform.gameObject.SetActive(false);
             GameManager.Instance.UIManager.OpenBookMenu();
             IsOpen = true;
             MainMenu.SetActive(true);
@@ -65,6 +67,7 @@ public class BookMenu : Menu
         activeMenu = null;
         GameManager.Instance.AudioManager.PlaySound(AudioRef.CloseMenu);
         Time.timeScale = 1.0f;
+        GameManager.Instance.PlayerTransform.gameObject.SetActive(true);
     }
 
     public void OpenSettingsMenu()
